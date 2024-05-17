14ded9bdabc982ae3acc2086e654ed6f04533545
pratham025 <prathamerit@gmail.com>
2024-05-17 23:53:46 +0530

    Started a new ChatBot

---
+++ app/src/main/java/com/example/chathurbot/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.chathurbot;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    TextView textView;
+    EditText editText;
+    ImageButton imageButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        messageList = new ArrayList<>();
+        recyclerView = findViewById(R.id.recycler_view);
+        textView = findViewById(R.id.welcome_text);
+        editText = findViewById(R.id.message_edit_text);
+        imageButton = findViewById(R.id.send_btn);
+
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+        imageButton.setOnClickListener((v)->{
+            String question = editText.getText().toString().trim();
+            addToChat(question, Message.SENT_BY_ME);
+            editText.setText("");
+            textView.setVisibility(View.GONE);
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    void addToChat(String message, String sentBy){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message,sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+
+    }
+
+}
+
+
+
+
